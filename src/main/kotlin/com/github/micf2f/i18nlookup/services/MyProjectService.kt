a03650a127cbab9054bb30cfb5f9a1adc95a0fbd
GitHub Action <action@github.com>
2026-05-25 15:05:01 +0000

    Template cleanup

--- src/main/kotlin/com/github/micf2f/i18nlookup/services/MyProjectService.kt
+++ src/main/kotlin/com/github/micf2f/i18nlookup/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.micf2f.i18nlookup.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.micf2f.i18nlookup.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
